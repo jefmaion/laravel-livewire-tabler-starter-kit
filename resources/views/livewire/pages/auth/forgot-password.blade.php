48c8993a154697e42f4112aaf6681719219b0133
Jefferson Maion de Oliveira <you@example.com>
2025-12-04 15:20:16 -0300

    change forgot password page to tabler.io

--- resources/views/livewire/pages/auth/forgot-password.blade.php
+++ resources/views/livewire/pages/auth/forgot-password.blade.php
@@ -35,34 +35,6 @@ new #[Layout('layouts.guest')] class extends Component
         session()->flash('status', __($status));
     }
 }; ?>
-{{--
-<div>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password
-        reset link that will allow you to choose a new one.') }}
-    </div>
-
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form wire:submit="sendPasswordResetLink">
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required
-                autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</div> --}}
-
-
 
 <div>
     <h2 class="card-title text-center mb-4">Forgot password</h2>
@@ -79,13 +51,7 @@ new #[Layout('layouts.guest')] class extends Component
     </div>
     <div class="form-footer">
         <button type="submit" class="btn btn-primary btn-4 w-100">
-            <!-- Download SVG icon from http://tabler.io/icons/icon/mail -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                class="icon icon-2">
-                <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
-                <path d="M3 7l9 6l9 -6" />
-            </svg>
+            <x-icons.mail />
             {{ __('Email Password Reset Link') }}
         </button>
     </div>

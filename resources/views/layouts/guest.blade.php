13d7a112f4f5d2397a8ef9747ae91a9f5ec98354
unknown <jefmaion@hotmail.com>
2025-12-08 22:37:25 -0300

    acp

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,25 +4,32 @@
 <head>
     @include('layouts.parts.header')
 </head>
+<style>
+    .navbar-brand-image-login {
+        height: 4rem;
+        width: auto;
+    }
+</style>
 
 <body>
     @include('layouts.parts.theme')
     <div class="page page-center">
         <div class="container container-tight py-4">
-            <div class="text-center mb-4">
-                <a href="." wire:navigate class="navbar-brand navbar-brand-autodark">
-                    <x-application-logo />
-                </a>
-            </div>
+
             <div class="card card-md">
                 <div class="card-body">
+                    <div class="text-center mb-4">
+                        <a href="." wire:navigate class="navbar-brand navbar-brand-autodark">
+                            <x-application-logo />
+                        </a>
+                    </div>
                     {{ $slot }}
                 </div>
             </div>
             {{ $link ?? null }}
         </div>
     </div>
-   @include('layouts.parts.scripts')
+    @include('layouts.parts.scripts')
 </body>
 
 </html>

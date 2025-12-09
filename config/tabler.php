84d978058f6e412956392413c629b420b9fce9ae
Jefferson Maion de Oliveira <you@example.com>
2025-12-09 12:23:13 -0300

    wip

--- config/tabler.php
+++ config/tabler.php
@@ -6,10 +6,10 @@ return [
 
     'theme-config' => [
         'theme'         => 'light',      /** light, dark */
-        'theme-base'    => 'stone',      /** slate, gray, zinc, neutral, stone */
+        'theme-base'    => 'neutral',      /** slate, gray, zinc, neutral, stone */
         'theme-font'    => 'sans-serif', /** sans-serif, serif, monospace, comic */
-        'theme-primary' => 'purple',     /** blue, azure, indigo, purple, pink, red, orange, yellow, lime, green, teal,m cyan    */
-        'theme-radius'  => '0.5',        /** 0, 0.5, 1, 1.5, 2 */
+        'theme-primary' => 'azure',     /** blue, azure, indigo, purple, pink, red, orange, yellow, lime, green, teal,m cyan    */
+        'theme-radius'  => '1.5',        /** 0, 0.5, 1, 1.5, 2 */
     ],
 
     'sidebar-menu' => [
